a7094ee6c09fa6d6b586d16c76ee8be4c8fcad4f
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-03-09 21:28:57 +0500

    Add Icons for Events

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const events = [
+    {
+      title: 'Remembering Faiz',
+      icon:
+        '',
+      description:
+        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+    },
+    {
+      title: 'Ghazal Night',
+      icon:
+        '',
+      description:
+        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+    },
+    {
+      title: 'Politcal Debates',
+      icon:
+        '',
+      description:
+        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+    },
+    {
+      title: 'Cultural Meetup',
+      icon:
+        '',
+      description:
+        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+    },
+    {
+      title: 'Music Night',
+      icon:
+        '',
+      description:
+        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+    },
+]
