06f71e60a00948d9ccd7cc8f296e667be6ecf2a9
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-03-09 22:04:44 +0500

    Change the colors of the Icons Events

--- js/main.js
+++ js/main.js
@@ -1,83 +1,71 @@
 const events = [
-    {
-      title: 'Remembering Faiz',
-      icon:
-        '',
-      description:
-        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-    },
-    {
-      title: 'Ghazal Night',
-      icon:
-        '',
-      description:
-        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-    },
-    {
-      title: 'Politcal Debates',
-      icon:
-        '',
-      description:
-        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-    },
-    {
-      title: 'Cultural Meetup',
-      icon:
-        '',
-      description:
-        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-    },
-    {
-      title: 'Music Night',
-      icon:
-        '',
-      description:
-        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-    },
-]
+  {
+    title: "Remembering Faiz",
+    icon: "",
+    description:
+      "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+  },
+  {
+    title: "Ghazal Night",
+    icon: "",
+    description:
+      "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+  },
+  {
+    title: "Politcal Debates",
+    icon: "",
+    description:
+      "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+  },
+  {
+    title: "Cultural Meetup",
+    icon: "https://raw.githubusercontent.com/RaoAkif/ConferencePage/79b65169e507764c21c6e8376885a7563f5e6250/images/events/cultural_meetup.svg",
+    description:
+      "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+  },
+  {
+    title: "Music Night",
+    icon: "",
+    description:
+      "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+  },
+];
 
 const speakers = [
-    {
-      name: 'Remembering Faiz',
-      picture:
-        '',
-      bio:
-        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-    },
-    {
-      name: 'Ghazal Night',
-      picture:
-        '',
-      bio:
-        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-    },
-    {
-      name: 'Politcal Debates',
-      picture:
-        '',
-      bio:
-        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-    },
-    {
-      name: 'Cultural Meetup',
-      picture:
-        '',
-      bio:
-        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-    },
-    {
-      name: 'Music Night',
-      picture:
-        '',
-      bio:
-        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-    },
-    {
-      name: 'Music Night',
-      picture:
-        '',
-      bio:
-        "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-    },
-]
-
+  {
+    name: "Zia Moheyuddin",
+    picture:
+      "https://raw.githubusercontent.com/RaoAkif/ConferencePage/main/images/speakers/zia_moheyuddin.jpg",
+    bio: "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+  },
+  {
+    name: "Moneza Hashmi",
+    picture:
+      "https://raw.githubusercontent.com/RaoAkif/ConferencePage/main/images/speakers/moneza-hashmi.jpg",
+    bio: "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+  },
+  {
+    name: "Tina Sani",
+    picture:
+      "https://raw.githubusercontent.com/RaoAkif/ConferencePage/main/images/speakers/tina-sani.jpg",
+    bio: "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+  },
+  {
+    name: "Naseeruddin Shah",
+    picture:
+      "https://raw.githubusercontent.com/RaoAkif/ConferencePage/main/images/speakers/naseeruddin-shah.jpg",
+    bio: "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+  },
+  {
+    name: "Samina Peerzada",
+    picture:
+      "https://raw.githubusercontent.com/RaoAkif/ConferencePage/main/images/speakers/samina-peerzada.jpg",
+    bio: "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+  },
+  {
+    name: "Samia Mumtaz",
+    picture:
+      "https://raw.githubusercontent.com/RaoAkif/ConferencePage/main/images/speakers/samia-mumtaz.jpg",
+    bio: "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
+  },
+];
