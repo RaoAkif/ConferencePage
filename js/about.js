33beb599c31d75c860e4179008f05392fadaffb2
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-03-10 18:21:30 +0500

    Customize Content of Website

--- js/about.js
+++ js/about.js
@@ -1,36 +1,3 @@
-const events = [
-  {
-    title: "Faiz Talk",
-    icon: "https://raw.githubusercontent.com/RaoAkif/ConferencePage/06f71e60a00948d9ccd7cc8f296e667be6ecf2a9/images/events/remembering_faiz.svg",
-    description:
-      "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-  },
-  {
-    title: "Ghazal Night",
-    icon: "https://raw.githubusercontent.com/RaoAkif/ConferencePage/06f71e60a00948d9ccd7cc8f296e667be6ecf2a9/images/events/ghazal_night.svg",
-    description:
-      "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-  },
-  {
-    title: "Political Debates",
-    icon: "https://raw.githubusercontent.com/RaoAkif/ConferencePage/06f71e60a00948d9ccd7cc8f296e667be6ecf2a9/images/events/political_debate.svg",
-    description:
-      "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-  },
-  {
-    title: "Cultural Meetup",
-    icon: "https://raw.githubusercontent.com/RaoAkif/ConferencePage/06f71e60a00948d9ccd7cc8f296e667be6ecf2a9/images/events/cultural_meetup.svg",
-    description:
-      "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-  },
-  {
-    title: "Music Night",
-    icon: "https://raw.githubusercontent.com/RaoAkif/ConferencePage/06f71e60a00948d9ccd7cc8f296e667be6ecf2a9/images/events/music_night.svg",
-    description:
-      "Listen to the Poets and their beautiful poetry that would enlighten your spirit and revive the era of Faiz Ahmad Faiz.",
-  },
-];
-
 const speakers = [
   {
     name: "Zia Moheyuddin",
@@ -99,33 +66,11 @@ const partners = [
   },
 ];
 
-const Events = document.querySelector("#events-list");
+const Partners = document.querySelector(".partners-list");
 
-events.forEach((event, index) => {
-  Events.innerHTML += `
-  <li class="event">
-  <img class="event-icon" src=${event.icon} alt="event-icon-${index}" >
-  <h4 class="event-name txt-c">${event.title}</h4>
-  <p class="event-desc">${event.description}</p>
-  </li>
-`;
-});
-
-const Speakers = document.querySelector("#speakers-list");
-
-speakers.forEach((speaker, index) => {
-  Speakers.innerHTML += `
-    <li class="speaker">
-        <div class="speaker-image">
-            <img src="https://raw.githubusercontent.com/RaoAkif/ConferencePage/6a691a0e99a362752772e4926d17ed4943e33517/images/bg/speaker_bg.svg" alt="speaker background" class="speaker-image-bg">
-            <img src=${speaker.picture} alt="speaker profile pic${index}" class="speaker-pic">
-        </div>
-        <div class="speaker-desc">
-            <div class="speaker-name">${speaker.name}</div>
-            <div class="speaker-position">${speaker.position}</div>
-            <div class="speaker-bio">${speaker.name} is an one of the oldest PTV actoress, writer and director, and serving this industry from more than 40 years.</div>
-        </div>
-    </li>
+partners.forEach((partner, index) => {
+  Partners.innerHTML += `
+    <li class="partner"><img src=${partner.logo} alt="spaceX-${index}" class="partner-img"></li>
 `;
 });
 
